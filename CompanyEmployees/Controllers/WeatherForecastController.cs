9fa9e06ae905d8e2b48b88b691893ba95ac479d1
souts <souts.jjw@gmail.com>
2021-09-05 17:06:48 +0800

    Chapter 1 ~ Chapter 13
    Chapter 1 ~ Chapter 13

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+
+        public WeatherForecastController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+
+        public Company GetCompany(Guid companyId, bool trackChanges) =>
+            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+            .SingleOrDefault();
+
+        public void CreateCompany(Company company) => Create(company);
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+            .ToList();
+
+        public void DeleteCompany(Company company) => Delete(company);
+    }
+}
